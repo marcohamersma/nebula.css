4e7c120ec079804988b1d0b9287fca9897a21099
marcohamersma <hello@marcohamersma.nl>
2015-04-11 15:28:06 +0200

    Initial WIP setup

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                },
+                files: {
+                    "css/styles.css": "scss/styles.scss"
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: false,
+            },
+            styles: {
+                files: ['scss/**/*.scss'], // which files to watch
+                tasks: ['sass'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['sass', 'watch']);
+};
