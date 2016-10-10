6c810aeffc50cc6667c2494657298c2497ac2a63
marcohamersma <hello@marcohamersma.nl>
2016-10-10 18:38:24 +0200

    Removing documentation + readme building from this branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,13 @@
 var builder = require('./lib/nebula-builder');
 var baseConfig = require('./lib/base-config');
 var configBuilder = require('./lib/config-builder');
-var readmeBuilder = require('./lib/readme-builder');
 
 module.exports = function(grunt) {
   grunt.initConfig({
     watch: {
       styles: {
-        files:   ['scss/**/*.scss', 'readme-src/*', 'lib/*'], // which files to watch
-        tasks:   ['custom', 'buildReadme'],
+        files:   ['scss/**/*.scss', 'lib/*'], // which files to watch
+        tasks:   ['custom'],
         options: {
           nospawn: true
         }
@@ -17,7 +16,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['buildVanilla', 'buildReadme', 'watch']);
+  grunt.registerTask('default', ['buildVanilla', 'watch']);
 
   grunt.registerTask('buildVanilla', function() {
     var modules = baseConfig.defaultModules;
@@ -33,24 +32,4 @@ module.exports = function(grunt) {
   grunt.registerTask('buildConfig', function() {
     grunt.file.write('./scss/nebula/config.scss', configBuilder(baseConfig.variables));
   });
-
-  grunt.registerTask('buildReadme', ['readme-html', 'readme-markdown']);
-
-  grunt.registerTask('readme-html', 'Compile the index.html for Github Pages, plus generate styles', function() {
-    var done = this.async();
-    readmeBuilder.html(function(result) {
-      grunt.file.write('./index.html', result);
-      done();
-    });
-  });
-
-  grunt.registerTask('readme-markdown', 'Generate the markdown from the from the HTML', function() {
-    var done = this.async();
-    readmeBuilder.markdown(function(result) {
-      grunt.file.write('./README.MD', result);
-      done();
-    });
-  });
-
-
 };
