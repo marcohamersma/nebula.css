19ab5c4229dea5f59e4d87ad9e0881eaacc490b7
marcohamersma <hello@marcohamersma.nl>
2016-10-10 20:56:38 +0200

    add a comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,9 @@ module.exports = function(grunt) {
     builder(modules, './dist/nebula.css', null, done);
   });
 
+  // This task creates a sass file from the base-config.js file,
+  // used for developing nebula and making it easy to copy configuration from
+  // the js-based build tools to the sass based system.
   grunt.registerTask('buildConfig', function() {
     grunt.file.write('./scss/_config.scss', configBuilder(baseConfig.variables));
   });
