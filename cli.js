8a9d80150f13a2bb52c01662ab8faded39b58695
marcohamersma <hello@marcohamersma.nl>
2016-10-16 18:04:01 +0200

    update CLI

--- cli.js
+++ cli.js
@@ -4,11 +4,17 @@ var camelize = require('camelize');
 var nebula = require('./index');
 var modules = require('./lib/base-config').defaultModules.filter( m => m !== 'banner');
 
-program.arguments('<entryFile>')
+function list(val) {
+  return val.split(',');
+}
+
+program
   .usage('<entryFile> [options]')
+  .arguments('<entryFile>')
   .option('-o, --output <path>', 'Path to output file, stdout will be used otherwise')
   .option('--noSourceMap', 'Do not generate a sourcemap file alongside the file defined in --output');
 
+// Add an option for all Nebula's default modules
 modules.forEach( moduleName => {
   var option = camelize('no-' + moduleName);
   const message = 'Do not include ' + moduleName.replace(/-?classes/, '') + ' classes in the output css';
@@ -16,36 +22,38 @@ modules.forEach( moduleName => {
   program.option('--' + option, message);
 });
 
-function list(val) {
-  return val.split(',');
-}
-
+// Add other options
 program
   .option('-m, --modules [modules]', 'comma separated list of nebula modules to use', list)
   .option('--noBanner', 'Hide the “Powered by nebula.css…” banner from output css')
   .action(function(entryFile) {
-    const nebulaConfig = {
-      outFile: program.output,
-      modules: program.modules
-    };
-
-    modules.forEach( m => {
-      const settingForModule = program[camelize('no-' + m)];
-      if (settingForModule) {
-        nebulaConfig['use-' + m] = !settingForModule;
-      }
-    });
-
-    nebula.build(entryFile, nebulaConfig)
-      .then(output => {
-        if (nebulaConfig.outFile) {
-          console.log('Written to ' + output);
-        } else {
-          process.stdout.write(output);
-        }
-      }).catch( err => {
-        console.error(err);
-        process.exit(1);
-      });
+    // Make sure we save the entryFile somewhere if it's passed
+    program.entryFile = entryFile;
   })
   .parse(process.argv);
+
+const nebulaConfig = {
+  outFile: program.output,
+  modules: program.modules
+};
+
+modules.forEach( m => {
+  // If passed, invert and transform options like noReset to useReset
+  const settingForModule = program[camelize('no-' + m)];
+  if (settingForModule) {
+    nebulaConfig['use-' + m] = !settingForModule;
+  }
+});
+
+// Go ahead and build
+nebula.build(program.entryFile, nebulaConfig)
+  .then(output => {
+    if (nebulaConfig.outFile) {
+      console.log('Written to ' + output);
+    } else {
+      console.log(output);
+    }
+  }).catch( err => {
+    console.log(err);
+    process.exit(1);
+  });
