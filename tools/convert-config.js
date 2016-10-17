c98bc60be56e363a6f035d624a71a94d059c73f4
marcohamersma <hello@marcohamersma.nl>
2016-10-17 13:08:45 +0200

    Convert grunt buildConfig task to a script

---
+++ tools/convert-config.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+var configBuilder = require('../lib/config-builder');
+var baseConfig = require('../lib/base-config');
+
+// This script creates a sass file from the base-config.js file,
+// used for developing nebula and making it easy to copy configuration from
+// the js-based build tools to the sass based system.
+
+fs.writeFileSync(
+  path.join(__dirname, '../scss/_config.scss'),
+  configBuilder(baseConfig.variables)
+);
