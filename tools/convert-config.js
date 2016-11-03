9045a4e53385b3849f65b98e01a7905eff1e942e
marcohamersma <hello@marcohamersma.nl>
2016-11-03 13:12:29 +0100

    baseConfig is now camelCased

--- tools/convert-config.js
+++ tools/convert-config.js
@@ -1,13 +1,12 @@
 var fs = require('fs');
 var path = require('path');
 var configBuilder = require('../lib/config-builder');
-var baseConfig = require('../lib/base-config');
+var sassVariables = require('../lib/sass-variables');
 
 // This script creates a sass file from the base-config.js file,
 // used for developing nebula and making it easy to copy configuration from
 // the js-based build tools to the sass based system.
-
 fs.writeFileSync(
   path.join(__dirname, '../scss/_config.scss'),
-  configBuilder(baseConfig.variables)
+  configBuilder(sassVariables)
 );
